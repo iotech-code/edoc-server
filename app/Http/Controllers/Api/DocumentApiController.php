95dd519f10eb0e262e6b4abf3c35e60a370ff9b7
Monopeelz <monopeelz@gmail.com>
2018-11-16 18:19:08 +0700

    commit test

--- app/Http/Controllers/Api/DocumentApiController.php
+++ app/Http/Controllers/Api/DocumentApiController.php
@@ -54,7 +54,7 @@ class DocumentApiController extends BaseApiController
     }
 
     public function comment($id, Request $request) {
-        return $request->file('files');
+        return $request->files;
         $user = auth()->user();
         $documnet = Document::where('school_id', $user->school_id)->find($id);
 
