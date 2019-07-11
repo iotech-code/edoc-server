2fd60230cc4eb90bcaf87349b9747ca037727851
Apinan <apinan@iotech.co.th>
2019-07-11 04:16:53 +0000

    Update resources/views/officers/index.blade.php

--- resources/views/officers/index.blade.php
+++ resources/views/officers/index.blade.php
@@ -28,7 +28,7 @@
   
     </div>
     <div class="row">
-      <div class="col-8 offset-2">
+      <div class="col-12">
         <div class="card">
           <div class="card-body p-0 border-top-primary">
             <table class="table table-striped">
