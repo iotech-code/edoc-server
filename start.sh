a133960a037a0c6f46c33bf1765f0a0c8a52375c
Apinan Woratrakun <iamapinan@gmail.com>
2022-02-21 17:52:09 +0700

    update docker

--- start.sh
+++ start.sh
@@ -2,9 +2,9 @@
 
 echo "Starting Getters..."
 echo "Linking storage"
-php /var/www/artisan storage:link
+php /var/www/html/artisan storage:link
 touch /var/www/storage/logs/laravel.log
-chmod 777 -R /var/www/storage/logs /var/www/storage/framework /var/www/storage/app /var/www/bootstrap/cache
+chmod 777 -R /var/www/html/storage/logs /var/www/html/storage/framework /var/www/html/storage/app /var/www/html/bootstrap/cache
 chown www-data:www-data storage/logs/laravel.log bootstrap/
 echo "Running service..."
 exec /usr/bin/supervisord -n -c /supervisord.conf
